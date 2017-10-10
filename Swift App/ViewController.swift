d4cdc414d648996338870a26ce87004ecd7fd7f6
Alykhan Bharmal <aly.b562@gmail.com>
2017-10-10 18:59:24 +0100

    My first commit.

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -10,9 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var buttonPresses = 0
+    @IBOutlet weak var theLabel: UILabel!
+    @IBAction func buttonPressed(_ sender: Any) {
+        
+        /* if theLabel.textColor == UIColor.red {
+            theLabel.textColor = UIColor.orange
+            theLabel.text = "I also sometimes..."
+        } else if theLabel.textColor == UIColor.orange {
+            theLabel.textColor = UIColor.yellow
+            theLabel.text = "... change colour."
+        } else {
+            theLabel.text = "But not always!"
+        }
+         */
+        
+        buttonPresses += 1
+       print("Button Clicked \(buttonPresses) times")
+        if buttonPresses >= 10 {
+            theLabel.text = "You pressed the button \(buttonPresses) times!"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        theLabel.text = "This Worked, I Think..."
+        theLabel.textColor = UIColor.red
+        self.view.backgroundColor = UIColor.black
+        
     }
 
     override func didReceiveMemoryWarning() {
